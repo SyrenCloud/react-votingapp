d3a507ed7886218f224b59ece07c3b37302e55cd
nagendrasyn21 <82216150+nagendrasyn21@users.noreply.github.com>
2021-04-13 22:41:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,30 +21,5 @@ app.get('/votes', (req, res) => {
   res.send('Hello World Version votes');
 });
 
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, '../my-app/build')));
-
-app.get('/api/votes', (req, res) => {
-  console.log('api/votes called!')
-  res.json(votes);
-});
-
-app.get('/api/resetvotes', (req, res) => {
-  console.log('api/resetvotes called!')
-  votes.slice(0,votes.length-1);
-  res.json(votes);
-});
-
-app.post('/api/vote', (req, res) => {
-  const vote = req.body.vote;
-  console.log('Adding vote:::::', vote);
-  votes.push(vote);
-  res.json("vote recorded");
-});
-
-app.get('/index', (req,res) => {
-  res.sendFile(path.join(__dirname, '../my-app/build/index.html'));
-});
-
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
