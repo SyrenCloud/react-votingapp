4bb3a2cd498d24e05fdbf45dc80a66e4610d8566
nagendrasyn21 <82216150+nagendrasyn21@users.noreply.github.com>
2021-04-13 20:29:17 +0530

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,11 @@
-FROM node:10
-
+FROM node:10 AS ui-build
 WORKDIR /usr/src/app
+COPY my-app/ ./my-app/
+RUN cd my-app && npm install && npm run build
+
+FROM node:10 AS server-build
+WORKDIR /root/
+COPY --from=ui-build /usr/src/app/my-app/build ./my-app/build
 
 COPY package.json .
 
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 'use strict';
 
 const express = require('express');
+const path = require('path');
+const app = express(),
+      bodyParser = require("body-parser");
+
 
 // Constants
 const PORT = 8080;
@@ -10,6 +14,35 @@ const HOST = '0.0.0.0';
 const app = express();
 app.get('/', (req, res) => {
   res.send('Hello World Version 1');
+});
+
+
+// place holder for the data
+const votes = [];
+
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '../my-app/build')));
+
+app.get('/api/votes', (req, res) => {
+  console.log('api/votes called!')
+  res.json(votes);
+});
+
+app.get('/api/resetvotes', (req, res) => {
+  console.log('api/resetvotes called!')
+  votes.slice(0,votes.length-1);
+  res.json(votes);
+});
+
+app.post('/api/vote', (req, res) => {
+  const vote = req.body.vote;
+  console.log('Adding vote:::::', vote);
+  votes.push(vote);
+  res.json("vote recorded");
+});
+
+app.get('/api/index', (req,res) => {
+  res.sendFile(path.join(__dirname, '../my-app/build/index.html'));
 });
 
 app.listen(PORT, HOST);
